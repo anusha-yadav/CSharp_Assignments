9e211cbeba30f524d62a22e7030a80516d2ea6cf
anusha-yadav <anushayadav.1904f1@gmail.com>
2023-06-14 07:48:19 +0530

    added

---
+++ Three_Tier_Project/ConsoleApp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Text;
+using BusinessTier;
+class PresentationLayer
+{
+    public static void Main()
+    {
+        UserBL bL = new UserBL();
+        while (true)
+        {
+            Console.WriteLine("Please enter\n1. Login\n2. For Registeration");
+            string choice = Console.ReadLine();
+            int flag = 0;
+            switch (choice)
+            {
+                case "1":
+                    bL.LoginDisplay();
+                    break;
+                case "2":
+                    bL.Registration_Display();
+                    break;
+                default:
+                    break;
+            }
+        }
+    }
+}
---
+++ Three_Tier_Project/Three_Tier_Project/WebForm1.aspx.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data;
+using DataAccess;
+using BuisnessLogic;
+
+namespace Three_Tier_Project
+{
+    public partial class WebForm1 : System.Web.UI.Page
+    {
+        UserBL ba = new UserBL();
+        UserDA da = new UserDA();
+        DataTable dt = new DataTable();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            da.username = txtuname.Text;
+            da.password = txtpassword.Text;
+            dt = ba.loginuser(da);
+            if(dt.Rows.Count > 0)
+            {
+                Response.Redirect("home.aspx");
+            }
+            else
+            {
+                Response.Write("<script>alert('invalid username and password')</script>");
+            }
+        }
+    }
+}
