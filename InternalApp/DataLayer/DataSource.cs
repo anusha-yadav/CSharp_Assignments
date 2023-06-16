cd172d38320e8d02feb9d63a6bb52ac54416a2f8
anusha-yadav <anushayadav.1904f1@gmail.com>
2023-06-16 20:15:31 +0530

    Added

---
+++ InternalApp/DataLayer/DataSource.cs
@@ -0,0 +1,24 @@
+﻿using BusinessModels;
+
+namespace DataLayer
+{ 
+    /// <summary>
+    /// DataSource class which contains database
+    /// </summary>
+    internal class DataSource 
+    {
+        public static List<User> dataBase = new List<User>();
+        public void AddDetails(User user)
+        {
+            dataBase.Add(user);
+        }
+
+        public void GetDetails()
+        {
+            for (int i = 0; i < dataBase.Count; i++)
+            {
+                Literals.DisplayObjDetails(dataBase[i]);
+            }
+        }
+    }
+}
